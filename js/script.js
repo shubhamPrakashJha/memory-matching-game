ebd66bc3d978073bd22a5a0d337a744f8c0d3bac
Shubham Prakash Jha <prakash.shubhamjha@gmail.com>
2019-04-24 13:38:17 +0530

    after opening 2 cards, unihde cards permanentle if matched and hide if unmatched

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const deck = document.querySelector('.deck')
 
 const shuffleButton = document.querySelector('.restart');
 
+let openCards = [];
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 // takes a list as an input returns the shuffled list
 function shuffle(array) {
@@ -49,3 +51,59 @@ document.addEventListener('DOMContentLoaded', shuffleCard);
 // shuffle the card when reload gets clicked
 shuffleButton.addEventListener('click', reset);
 
+
+/*
+ * Add interactivity to the card
+ */
+// locak the card in the open position
+function lockIcon() {
+    for (const card of openCards) {
+        card.classList.add('match');
+        card.classList.remove('open', 'show');
+    }
+    openCards = [];
+}
+
+// hide cards after 200ms
+function hideIcons() {
+    setTimeout(function alret() {
+        for (const card of openCards) {
+            card.classList.remove('open', 'show');
+        }
+        openCards = [];
+    }, 200)
+
+}
+
+// after opening 2 cards hide the card if not matched, lock as open otherwise
+function openCardList(card) {
+    // add the card to open cards list
+    if (openCards[0] !== card) {
+        openCards.push(card);
+    }
+    // check the match of card after 2 cards is opened
+    if (openCards.length === 2) {
+        console.log("2 open");
+        if (openCards[0].childNodes[0].classList[1] == openCards[1].childNodes[0].classList[1]) {
+            console.log('Matched');
+            lockIcon();
+        } else {
+            console.log('Not Macthed');
+            hideIcons();
+        }
+
+    }
+}
+
+//  show the hidden card icon
+function showIcon(card) {
+    card.classList.add('open', 'show');
+    openCardList(card)
+}
+
+// call showIcon Func when card is clicked
+deck.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'LI') {
+        showIcon(event.target);
+    }
+})
